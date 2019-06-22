bfddef8b11ebe4e11ae9b1d207a15d8f86764749
Victor <zup@MacBook-Pro-de-zup.local>
2019-06-22 19:32:49 -0300

    finalizado agendamento

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,8 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
+const AvaliableController = require('./app/controllers/AvaliableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSucces = req.flash('success')
@@ -32,4 +34,9 @@ routes.use('/app', authMiddleware)
 routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.post('/app/appointments/new/:provider', AppointmentController.store)
+routes.get('/app/avaliable/:provider', AvaliableController.index)
+
 module.exports = routes
